be754b523da372831f7b203405ff89f4320089f3
Alejandro Cerezo <alce65@hotmail.es>
2023-10-18 10:17:16 +0200

    Add strictEquals function

---
+++ es/strict.equals.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-else-return */
+export const strictEquals = (a, b) => {
+  if (Object.is(a, b)) {
+    if (Object.is(a, NaN)) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  if (Object.is(a, b)) {
+    if (
+      (Object.is(a, 0) && Object.is(b, -0)) ||
+      (Object.is(a, -0) && Object.is(b, 0))
+    ) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+};
